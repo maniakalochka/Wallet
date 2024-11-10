96261ab1da0f701a5051612deb78c04e00811b4c
Maniakalochka <maniakalochka@gmail.com.com>
2024-11-10 16:47:16 +0300

    moving migration files to src and changes in env.py

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -3,15 +3,14 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 from alembic import context
-from src.models import user, wallet, transaction
+from models import user, wallet, transaction
 
-from src.backend.db import Base
+from backend.db import Base
 import sys
 import os
 
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
-)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+# Настройка логирования
 config = context.config
 fileConfig(config.config_file_name)
 
