01d39abe977f0bc8eda95e0537b045c182128d9e
Maniakalochka <maniakalochka@gmail.com.com>
2024-11-10 08:57:07 +0300

    Create Pydantic schemas for wallet

---
+++ src/schemas/wallet.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import List, Optional, Annotated
+
+class WalletBase(BaseModel):
+    balance: float = Field(0.0, example=100.0, title='Balance')
+    # currency: str = Field('RUB', example='RUB', title='Currency')
+
+
+class WalletCreate(WalletBase):
+    pass
+
+
+class WalletUpdate(WalletBase):
+    pass
+
+
