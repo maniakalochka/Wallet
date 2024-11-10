1698ddc0937b6f55aacfb416a0cd2801505e9631
Maniakalochka <maniakalochka@gmail.com.com>
2024-11-10 08:56:59 +0300

    Create Pydantic schemas for user

---
+++ src/schemas/user.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import List, Optional, Annotated
+
+
+class UserBase(BaseModel):
+    first_name: str = Field(..., example='Robert', title='First Name')
+    last_name: str = Field(..., example='Smith', title='Last Name')
+    email: EmailStr = Field(..., example='email@example.com', title='Email')
+    password: str = Field(..., example='password', title='Password')
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserUpdate(BaseModel):
+    email: Optional[EmailStr] = Field(..., example="email@example.com", title="Email")
+    password: Optional[str] = Field(..., example="qwerty", title="Password")
+
+
+
+
+
+
