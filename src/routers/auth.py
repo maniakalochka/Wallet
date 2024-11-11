88f421229527b0c3c57c7367be438434878ae546
Maniakalochka <maniakalochka@gmail.com.com>
2024-11-11 13:41:37 +0300

    добавлена проверка существующего пользователя при создании нового, исправлены функции аутентификации и создания токена

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -28,11 +28,18 @@ ALGORITHM = 'HS256'
 
 @router.post('/')
 async def create_user(db: Annotated[AsyncSession, Depends(get_db)], create_user: UserCreate):
+    existing_user = await db.scalar(select(User).where((User.username == create_user.username) | (User.email == create_user.email)))
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='User or email already exists'
+        )
     await db.execute(insert(User).values(first_name=create_user.first_name,
                                          last_name=create_user.last_name,
                                          username=create_user.username,
                                          email=create_user.email,
                                          hashed_password=bcrypt_context.hash(create_user.password),
+                                         is_active=True
                                          ))
     await db.commit()
     return {
@@ -40,25 +47,9 @@ async def create_user(db: Annotated[AsyncSession, Depends(get_db)], create_user:
         'transaction': 'Successful'
     }
 
-async def authanticate_user(db: Annotated[AsyncSession, Depends(get_db)], username: str, password: str):
-    user = await db.scalar(select(User).where(User.username == username))
-    if not user or not bcrypt_context.verify(password, user.hashed_password) or user.is_active == False:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return user
-
-async def create_access_token(username: str, user_id: int, is_admin: bool, expires_delta: timedelta):
-    encode = {'sub': username, 'id': user_id, 'is_admin': is_admin}
-    expires = datetime.now() + expires_delta
-    encode.update({'exp': expires})
-    return jwt.encode(encode, SECRET_TOKEN, algorithm=ALGORITHM)
-
 @router.post('/token')
 async def login(db: Annotated[AsyncSession, Depends(get_db)], form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user = await authanticate_user(db, form_data.username, form_data.password)
+    user = await authenticate_user(db, form_data.username, form_data.password)
 
     if not user or user.is_active == False:
         raise HTTPException(
@@ -69,7 +60,7 @@ async def login(db: Annotated[AsyncSession, Depends(get_db)], form_data: Annotat
     token = await create_access_token(user.username, user.id, user.is_admin, timedelta(minutes=20))
 
     return {
-        'access_token': user.username,
+        'access_token': token,
         'token_type': 'bearer'
     }
 
@@ -111,4 +102,21 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
 async def read_current_user(user: User = Depends(oauth2_scheme)):
     return user
 
+
+async def authenticate_user(db: Annotated[AsyncSession, Depends(get_db)], username: str, password: str):
+    user = await db.scalar(select(User).where(User.username == username))
+    if not user or not bcrypt_context.verify(password, user.hashed_password) or user.is_active == False:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def create_access_token(username: str, user_id: int, is_admin: bool, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id, 'is_admin': is_admin}
+    expires = datetime.now() + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_TOKEN, algorithm=ALGORITHM)
 
