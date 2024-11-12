93acd6244cdc2761a7cfc25c93004d2fb7a6449a
Maniakalochka <maniakalochka@gmail.com.com>
2024-11-12 13:10:41 +0300

    add create wallet endpoint

--- src/routers/wallet.py
+++ src/routers/wallet.py
@@ -1,1 +1,21 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from schemas.wallet import WalletCreate
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from database.db import get_db
+from repositories.wallet import WalletRepo
+
+
+router = APIRouter(prefix='/wallet', tags=['wallet'])
+
+
+@router.post('/')
+async def add_wallet(
+    wallet: WalletCreate,
+):
+    wallet_dict = wallet.model_dump()
+    wallet_id = await WalletRepo().add_one(wallet_dict)
+    return {
+        "wallet_id": wallet_id
+    }
+    
