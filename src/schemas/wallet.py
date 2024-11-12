ffb5791e755b8e93a08f2d4950490338f0c3c06e
Maniakalochka <maniakalochka@gmail.com.com>
2024-11-12 13:11:11 +0300

    impove Wallet Schema (add user_id)

--- src/schemas/wallet.py
+++ src/schemas/wallet.py
@@ -3,6 +3,7 @@ from typing import List, Optional, Annotated
 
 
 class WalletBase(BaseModel):
+    user_id: int = Field(...)
     balance: float = Field(0.0, example=100.0, title="Balance")
     # currency: str = Field('RUB', example='RUB', title='Currency')
 
