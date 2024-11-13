6c39b2e1094684b510a050b59b3d278eb1ee32d5
Maniakalochka <maniakalochka@gmail.com.com>
2024-11-13 11:16:25 +0300

    update transaction model, add sender and receiver

--- src/models/transaction.py
+++ src/models/transaction.py
@@ -9,12 +9,14 @@ class TransactionTypeEnum(enum.Enum):
     OUTCOME = "OUTCOME"
 
 
-class Transction(Base):
+class Transaction(Base):
     __tablename__ = "transaction"
-
+    
     id = Column(Integer, primary_key=True)
-    wallet_id = Column(Integer, ForeignKey("wallet.id"))
-    wallet = relationship("Wallet", back_populates="transaction")
+    sender_wallet_id = Column(Integer, ForeignKey("wallet.id"), nullable=False)
+    receiver_wallet_id = Column(Integer, ForeignKey("wallet.id"), nullable=False)
+    sender_wallet = relationship("Wallet", foreign_keys=[sender_wallet_id], back_populates="sent_transactions")
+    receiver_wallet = relationship("Wallet", foreign_keys=[receiver_wallet_id], back_populates="received_transactions")
     amount = Column(Float, default=0.0)
     description = Column(String, nullable=True)
     type = Column(Enum(TransactionTypeEnum), default=TransactionTypeEnum.INCOME)
