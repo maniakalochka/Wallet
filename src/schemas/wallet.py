3e59afa73016b04fd49b8eb9764b853c66fd35ba
Maniakalochka <maniakalochka@gmail.com.com>
2024-11-17 13:58:08 +0300

    update wallet schema

--- src/schemas/wallet.py
+++ src/schemas/wallet.py
@@ -1,21 +1,21 @@
 from pydantic import BaseModel, EmailStr, Field
 from typing import List, Optional, Annotated
+from models.wallet import CurrencyEnum
 
 
 class WalletBase(BaseModel):
-    user_id: int = Field(...)
-    balance: float = Field(0.0, example=100.0, title="Balance")
-    # currency: str = Field('RUB', example='RUB', title='Currency')
-
-    class Config:
-        from_attributes = True
-
-class WalletCreate(WalletBase):
-    pass
-
-
-class WalletUpdate(BaseModel):
     currency: str = Field('RUB', example='RUB', title='Currency')
 
     class Config:
         from_attributes = True
+
+
+class WalletCreate(BaseModel):
+    currency: str = Field(CurrencyEnum.RUB, example='RUB', title='Currency')
+
+    class Config:
+        from_attributes = True 
+
+
+class WalletUpdate(WalletCreate):
+    pass
