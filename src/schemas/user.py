7302c10870646e25d149f7c7eaf67c9ee35ab814
Maniakalochka <maniakalochka@gmail.com.com>
2024-11-18 13:46:07 +0300

    Create UserRead pydantic model

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -26,10 +26,17 @@ class UserLogin(BaseModel):
     password: str = Field(..., example="password", title="Password")
 
 
+class UserRead(BaseModel):
+    id: int = Field(..., example=1, title="ID")
+    first_name: str = Field(..., example="Robert", title="First Name")
+    last_name: str = Field(..., example="Smith", title="Last Name")
+    username: str = Field(..., example="RobertSmith", title="Username")
+    email: EmailStr = Field(..., example="", title="Email")
+
+    class Config:
+        from_attrubutes = True
+
+
 class UserDeactivate(BaseModel):
     username: str = Field(..., example="RobertSmith", title="Username")
     is_active: bool = Field(..., example=False, title="Is Active")
-
-
-class SuperUserCreate(UserBase):
-    is_admin: bool = Field(..., example=True, title="Is Admin")
