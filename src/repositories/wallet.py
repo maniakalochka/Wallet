0b753f5163d968ca523361004a8549a887349c89
Maniakalochka <maniakalochka@gmail.com.com>
2024-11-25 01:49:02 +0300

    Overrided find_all method in wallet Repo

--- src/repositories/wallet.py
+++ src/repositories/wallet.py
@@ -1,6 +1,16 @@
 from utils.repository import SQLAlchemyRepository
 from models.wallet import Wallet
+from database.db import async_session
+from sqlalchemy import select
 
 
 class WalletRepo(SQLAlchemyRepository):
     model = Wallet
+
+    async def find_all(self, user_id):
+        async with async_session() as session:
+            async with session.begin():
+                stmt = select(self.model).where(self.model.user_id == user_id)
+                res = await session.execute(stmt)
+                wallets = res.scalars().all()
+                return wallets
